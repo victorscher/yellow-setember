cb2c0981f2295ec59efef407535c497962833591
Victor Scher <victorscher92@gmail.com>
2019-12-19 16:46:44 -0300

    feat(images): Posicionando e animando imagens

---
+++ js/images.js
@@ -0,0 +1,4 @@
+gsap.to(".image-1", {duration: 1, top: "30px"});
+gsap.to(".image-2", {duration: 1, left: "74px"});
+gsap.to(".image-3", {duration: 1, left: "1032px"});
+gsap.to(".image-4", {duration: 1, top: "382px"});
